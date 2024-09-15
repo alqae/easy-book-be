facdf38f782dc216a52b0336b8b1e1459afc0b40
Sebastian Martinez <js.martinez.sandino@gmail.com>
2024-09-14 23:08:52 -0500

    fix: table names on AddTimestamps migration

--- src/migrations/1726354582975-AddTimestamps.ts
+++ src/migrations/1726354582975-AddTimestamps.ts
@@ -1,40 +1,41 @@
-import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
 
 export class AddTimestamps1726354582975 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.addColumn('user', new TableColumn({
+    await queryRunner.addColumn('users', new TableColumn({
       name: 'createdAt',
       type: 'timestamp',
       default: 'CURRENT_TIMESTAMP',
     }));
 
-    await queryRunner.addColumn('user', new TableColumn({
-      name: 'updatedAt',
-      type: 'timestamp',
-      default: 'CURRENT_TIMESTAMP',
-      onUpdate: 'CURRENT_TIMESTAMP',
-    }));
-    await queryRunner.addColumn('token', new TableColumn({
-      name: 'createdAt',
-      type: 'timestamp',
-      default: 'CURRENT_TIMESTAMP',
-    }));
-
-    await queryRunner.addColumn('token', new TableColumn({
+    await queryRunner.addColumn('users', new TableColumn({
       name: 'updatedAt',
       type: 'timestamp',
       default: 'CURRENT_TIMESTAMP',
       onUpdate: 'CURRENT_TIMESTAMP',
     }));
 
-    await queryRunner.addColumn('service', new TableColumn({
+    await queryRunner.addColumn('tokens', new TableColumn({
       name: 'createdAt',
       type: 'timestamp',
       default: 'CURRENT_TIMESTAMP',
     }));
 
-    await queryRunner.addColumn('service', new TableColumn({
+    await queryRunner.addColumn('tokens', new TableColumn({
+      name: 'updatedAt',
+      type: 'timestamp',
+      default: 'CURRENT_TIMESTAMP',
+      onUpdate: 'CURRENT_TIMESTAMP',
+    }));
+
+    await queryRunner.addColumn('services', new TableColumn({
+      name: 'createdAt',
+      type: 'timestamp',
+      default: 'CURRENT_TIMESTAMP',
+    }));
+
+    await queryRunner.addColumn('services', new TableColumn({
       name: 'updatedAt',
       type: 'timestamp',
       default: 'CURRENT_TIMESTAMP',
@@ -46,10 +47,10 @@ export class AddTimestamps1726354582975 implements MigrationInterface {
     await queryRunner.dropColumn('user', 'createdAt');
     await queryRunner.dropColumn('user', 'updatedAt');
 
-    await queryRunner.dropColumn('token', 'createdAt');
-    await queryRunner.dropColumn('token', 'updatedAt');
+    await queryRunner.dropColumn('tokens', 'createdAt');
+    await queryRunner.dropColumn('tokens', 'updatedAt');
 
-    await queryRunner.dropColumn('service', 'createdAt');
-    await queryRunner.dropColumn('service', 'updatedAt');
+    await queryRunner.dropColumn('services', 'createdAt');
+    await queryRunner.dropColumn('services', 'updatedAt');
   }
 }
