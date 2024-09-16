cf9752c3ce4024f15aabec245de0c8f44d455082
Sebastian Martinez <js.martinez.sandino@gmail.com>
2024-09-15 21:38:04 -0500

    feat: setup delete account

--- src/controllers/profileController.ts
+++ src/controllers/profileController.ts
@@ -4,6 +4,7 @@ import bcrypt from 'bcrypt';
 import { cleanKeys, sendResponse } from '../utils';
 import { RequestWithUser } from '../types/express';
 import { AppDataSource } from '../data-source';
+import { UserStatus } from '../types/enums';
 import { User } from '../models';
 
 const userRepository = AppDataSource.getRepository(User);
@@ -32,7 +33,10 @@ export const updateProfile = async (req: RequestWithUser, res: Response): Promis
 export const updatePassword = async (req: RequestWithUser, res: Response): Promise<Response> => {
   const { oldPassword, newPassword } = req.body;
 
-  const { password: savedPassword } = await userRepository.findOne({ where: { email: req.user.email } });
+  const { password: savedPassword } = await userRepository.findOne({
+    where: { email: req.user.email },
+    select: ['password']
+  });
   const isValidOldPassword = await bcrypt.compare(oldPassword, savedPassword);
 
   if (!isValidOldPassword) {
@@ -48,6 +52,11 @@ export const updatePassword = async (req: RequestWithUser, res: Response): Promi
 export const updateRole = async (req: RequestWithUser, res: Response): Promise<Response> => {
   await userRepository.update({ email: req.user.email }, { role: req.body.role });
   return sendResponse(res, 'Role updated successfully', null, 200);
+};
+
+export const deleteProfile = async (req: RequestWithUser, res: Response): Promise<Response> => {
+  await userRepository.update({ email: req.user.email }, { status: UserStatus.INACTIVE });
+  return sendResponse(res, 'Profile deleted successfully', null, 200);
 };
 
 // export const inviteUser = (req: Request, res: Response): void => {
